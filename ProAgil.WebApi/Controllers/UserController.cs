0bc1569005cd84f0fb5e5d731278e5bb65d245ca
guilhermesabreu <guilhermeabreucc@gmail.com>
2020-02-06 17:48:56 -0300

    JWT em andamento

--- ProAgil.WebApi/Controllers/UserController.cs
+++ ProAgil.WebApi/Controllers/UserController.cs
@@ -1,9 +1,14 @@
+using System;
 using System.Threading.Tasks;
 using AutoMapper;
 using AutoMapper.Configuration;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProAgil.Domain.Identity;
+using ProAgil.WebApi.Dtos;
 
 namespace ProAgil.WebApi.Controllers
 {
@@ -27,9 +32,65 @@ namespace ProAgil.WebApi.Controllers
             _userManager = userManager;
         }
 
-        public async Task<IActionResult> GetUser()
+        [HttpGet("GetUser")]
+        public async Task<IActionResult> GetUser(UserDto userDto)
         {
-            return Ok(new User());
+            return Ok(userDto);
+        }
+
+        [HttpPost("Register")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(UserDto userDto)
+        {
+            try
+            {
+                var user = _mapper.Map<User>(userDto);
+                var result = await _userManager.CreateAsync(user, userDto.Password);
+                var userToReturn = _mapper.Map<UserDto>(user);
+                if(result.Succeeded)
+                {
+                    return Created("GetUser", userToReturn);
+                }
+                return BadRequest(result.Errors);
+            }
+            catch (System.Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Banco de Dados Falhou {ex.Message}");
+            }  
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(UserLoginDto userLogin)
+        {
+
+            try
+            {
+                var user = await _userManager.FindByNameAsync(userLogin.UserName);
+                var result = await _signInManager.CheckPasswordSignInAsync(user, userLogin.Password, false);
+
+                if(result.Succeeded)
+                {
+                    var appUser = await _userManager.Users
+                                  .FirstOrDefaultAsync(u => u.NormalizedUserName == userLogin.UserName.ToUpper());
+
+                    var userToReturn = _mapper.Map<UserLoginDto>(appUser);
+                    return Ok(new {
+                        token = GenerateJWToken(appUser).Result,
+                        user = userToReturn
+                    });
+                }
+                return Unauthorized();
+            }
+            catch (System.Exception ex)
+            {
+                
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Banco de Dados Falhou {ex.Message}");
+            }
+        }
+
+        private async Task<string> GenerateJWToken(User appUser)
+        {
+            return ""; 
         }
     }
 }
--- ProAgil.WebApi/Helpers/AutoMapperProfiles.cs
+++ ProAgil.WebApi/Helpers/AutoMapperProfiles.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using AutoMapper;
 using ProAgil.Domain;
+using ProAgil.Domain.Identity;
 using ProAgil.WebApi.Dtos;
 
 namespace ProAgil.WebApi.Helpers
@@ -26,6 +27,9 @@ namespace ProAgil.WebApi.Helpers
             // Mapeamento normal, sem Forgein Key
             CreateMap<Lote, LoteDto>().ReverseMap();
             CreateMap<RedeSocial, RedeSocialDto>().ReverseMap();
+
+            CreateMap<User, UserDto>().ReverseMap();
+            CreateMap<User, UserLoginDto>().ReverseMap();
         }
     }
 }
