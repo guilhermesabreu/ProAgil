cdd1db0044fbea9e3af2f6b584da178caa479a36
guilhermesabreu <guilhermeabreucc@gmail.com>
2020-02-07 17:14:55 -0300

    Register User, em andamento

--- ProAgil.WebApi/Controllers/UserController.cs
+++ ProAgil.WebApi/Controllers/UserController.cs
@@ -60,6 +60,7 @@ namespace ProAgil.WebApi.Controllers
         }
 
         [HttpPost("Login")]
+        [AllowAnonymous]
         public async Task<IActionResult> Login(UserLoginDto userLogin)
         {
 
@@ -90,7 +91,35 @@ namespace ProAgil.WebApi.Controllers
 
         private async Task<string> GenerateJWToken(User appUser)
         {
-            return ""; 
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.UserName)
+            };
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.ASCII
+                    .GetBytes(_config.GetSection("AppSettings:Token").value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
