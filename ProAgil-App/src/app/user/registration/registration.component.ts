6954271cdd5bccc1549b847f6644b3bb09a03397
guilhermesabreu <guilhermeabreucc@gmail.com>
2020-02-12 14:14:17 -0300

    Método: confirmação de senha, implementado

--- ProAgil-App/src/app/user/registration/registration.component.ts
+++ ProAgil-App/src/app/user/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-registration',
@@ -11,6 +12,7 @@ export class RegistrationComponent implements OnInit {
   registerForm: FormGroup;
 
   constructor(public fb: FormBuilder
+            , private toastr: ToastrService
             ) {}
 
   ngOnInit() {
@@ -21,12 +23,26 @@ export class RegistrationComponent implements OnInit {
     this.registerForm = this.fb.group(
       {
         fullName: ['', Validators.required],
-        email: ['', Validators.required, Validators.email],
+        email: ['', [Validators.required, Validators.email]],
         userName: ['', Validators.required],
-        password: ['', Validators.required, Validators.minLength(4)],
-        confirmPassword: ['', Validators.required]
+        passwords : this.fb.group({
+          password: ['', [Validators.required, Validators.minLength(4)]],
+          confirmPassword: ['', Validators.required]
+        }, {validator: this.compararSenha})
       }
     );
+  }
+
+  compararSenha(fb: FormGroup){
+    const confirmSenhaCtrl = fb.get('confirmPassword');
+    if(confirmSenhaCtrl.errors === null || 'mismatch' in confirmSenhaCtrl.errors){
+      if (fb.get('password').value !== confirmSenhaCtrl.value) {
+        confirmSenhaCtrl.setErrors({mismatch: true});
+      }
+      else {
+        confirmSenhaCtrl.setErrors(null);
+      }
+    }
   }
 
   cadastrarUsuario() {
